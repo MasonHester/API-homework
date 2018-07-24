eb1509b13aae2ebe35d80ca4a9c77872403c65a5
Mason <MasonHester7878@gmail.com>
2018-07-24 19:08:11 -0400

    Site properly receives data from GIPHY

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,9 +4,35 @@ $(document).ready(function() {
 
     var $buttonsCol = $(".buttonsCol");
 
-    for(i = 0; i < animalsArray.length; i++) {
-        var button = $("<button>");
-        button.addClass("animalButton").attr("data-animal", animalsArray[i]).text(animalsArray[i]);
-        $buttonsCol.append(button);
+    var $GIFsCol = $("GIFsCol");
+
+    var APIKey = "Pj7SF1CwRUBJYlaIY08NhriMy6ogXXDB";
+
+    function displayButtons() {        
+        for(i = 0; i < animalsArray.length; i++) {
+            var button = $("<button>");
+            button.addClass("animalButton").attr("data-animal", animalsArray[i]).text(animalsArray[i]);
+            $buttonsCol.append(button);
+        }
     }
+
+    function displayGIFs() {
+        var animal = $(this).attr("data-animal");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + APIKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+    }
+
+    $buttonsCol.on("click", ".animalButton", displayGIFs)
+
+    
+
+
+    displayButtons();
 });
