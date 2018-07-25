b55faf79ec213faf4e2f1cbe2f5bba5d6ab25d2c
Mason <MasonHester7878@gmail.com>
2018-07-25 13:06:12 -0400

    Images properly append

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
-    animalsArray = ["Turtle", "Dog", "Puma", "Fox"];
+    animalsArray = ["Turtle", "Dog", "PufferFish", "Fox"];
 
     var $buttonsCol = $(".buttonsCol");
 
-    var $GIFsCol = $("GIFsCol");
+    var $GIFsCol = $(".GIFsCol");
 
     var APIKey = "Pj7SF1CwRUBJYlaIY08NhriMy6ogXXDB";
+
+    var result = {};
 
     function displayButtons() {        
         for(i = 0; i < animalsArray.length; i++) {
@@ -17,22 +19,43 @@ $(document).ready(function() {
     }
 
     function displayGIFs() {
+        $GIFsCol.empty();
         var animal = $(this).attr("data-animal");
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + APIKey;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey +"&q=" + animal + "&limit=10&rating=pg-13";
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
+            result = response.data;
+            
+
+            for(i = 0; i < result.length; i++) {
+                var $animalImage = $("<img>");
+                var $animalDiv = $("<div>");
+                var $animalPar = $("<par>");
+                $animalImage.attr("src", result[i].images.fixed_height_still.url).attr("data-state", "static").attr("data-static-url", result[i].images.fixed_height_still.url).attr("data-moving-url", result[i].images.fixed_height.url).addClass("animalImage");
+                $animalPar.text("Rating: " + result[i].rating);
+                $animalDiv.append($animalPar).prepend($animalImage);
+                $GIFsCol.append($animalDiv);
+                console.log("image appended");
+            }
         });
     }
 
-    $buttonsCol.on("click", ".animalButton", displayGIFs)
+    // function imageCheck() {
+    //     currentImage = $(this)(".animalImage");
+    //     if(currentImage.attr("data-state" === "static")) {
+    //         currentImage.attr("data-state", "moving").attr("src", result.
 
-    
+    //     }
+    // }
 
+    $buttonsCol.on("click", ".animalButton", displayGIFs);
+
+    // $GIFsCol.on("clicK", ".animalImage", imageCheck);
 
     displayButtons();
 });
